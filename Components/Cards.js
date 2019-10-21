00887c2bf525941484f3481c94f0091470c8ebd7
Salvador Zaragoza <salvador.zaragoza@uabc.edu.mx>
2019-10-20 18:24:05 -0700

    Added validation for the two guesses of the player

--- Components/Cards.js
+++ Components/Cards.js
@@ -30,41 +30,81 @@ const sources = [...memes, ...memes];
 class Cards extends React.Component {
 
     state = {
-        shownCardsIndexes: [],
+        //allowPress: true,
+        //prevPressedCard: null,
+        shownCardsIndexes: [], //CARDS TO BE SHOWN
+        tries: [], //TRIES PER TURN (MAX 2)
+        correctGuesses: [],//CARDS GUESSED CORRECTLY(WILL ALWAYS BE SHOWN)
+        countPressed: 0, //NUMBER OF TRIES (MAX 2 THEN RESETED)
+        //update: true,
     };
 
     keyExtractor = (_item, index) => String(index);
-    
-    // onCardPress = (index) => () => {
-
-    // };
 
     renderCard = ({index, item}) => {
         const onCardPress = () => {
-            this.setState({
-                shownCardsIndexes: [...this.state.shownCardsIndexes, index],
-            });
+            // if(this.state.prevPressedCard){
+            //     if (item === this.state.prevPressedCard){
+            //         setTimeout(() => {
+            //             this.setState({
+            //                 allowPress: true,
+            //                 shownCardsIndexes: this.state.shownCardsIndexes.filter((idx) => {
+            //                     return idx !== index;
+            //                 }),
+            //             });
+            //         }, 3000);
+            //     } else {    }
+            // }
+
+            if(!this.state.shownCardsIndexes.includes(index)){ //VALIDATE THAT THE IMAGES CAN ONLY BE PRESSED ONCE
+                if(this.state.countPressed < 2){ //VALIDATE THE NUMBER OF TRIES IS LESS THAN 2
+                    console.log('Touch ' + (this.state.countPressed+1));
+                    this.setState({
+                        shownCardsIndexes: [...this.state.shownCardsIndexes, index],
+                        countPressed: this.state.countPressed + 1,
+                        tries: [...this.state.tries, item-1], //ITEMS DIFFER BY 1 FROM THE INDEXES
+                    });
+                }
+
+                if(this.state.countPressed == 2){//WHEN THE USER GUESSES THE TWO CARDS THEN THE THIRD PRESS WILL REVEAL THE RESULTS
+                    if(this.state.tries[0] === this.state.tries[1]){
+                        console.log('Guessed correctly');
+                        this.setState({
+                            correctGuesses: [...this.state.correctGuesses, this.state.tries[0], (this.state.tries[1] + 10)],
+                            countPressed: 0,
+                            tries: [],
+                        }, () => this.setState({
+                            shownCardsIndexes: this.state.correctGuesses,
+                        }));
+                    } else {
+                        console.log('Guessed Incorrectly');
+                        this.setState({
+                            countPressed: 0,
+                            tries: [],
+                        },() => this.setState({
+                            shownCardsIndexes: this.state.correctGuesses,
+                        }));
+                    }
+                } 
+            }
         };
         
         return (
         <TouchableWithoutFeedback onPress = {onCardPress}>
             <View>
-            <Card 
-                shown= {this.state.shownCardsIndexes.includes(index)} 
-                source={item} 
-            />
+                <Card 
+                    shown = {this.state.shownCardsIndexes.includes(index)} 
+                    source={item} 
+                />
             </View>
         </TouchableWithoutFeedback>
         );
     };
 
-
-
-
     render() {
         return (
             <FlatList
-                contentContainerStyle= {styles.cardsContainer}
+                contentContainerStyle = {styles.cardsContainer}
                 data = {sources}
                 extraData = {this.state.shownCardsIndexes}
                 keyExtractor = {this.keyExtractor}
@@ -73,9 +113,6 @@ class Cards extends React.Component {
             />
         )
     }
-
-
-
 }
 
 const styles = StyleSheet.create({
